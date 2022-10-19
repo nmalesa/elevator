5a7f63cddb37addde8faaa6aa87fe4cb0c69050c
Natalia Malesa <nmalesa@gmail.com>
2022-10-19 18:50:15 -0500

    fix: Handle given scenario and case where both passengers are going up

--- elevator.js
+++ elevator.js
@@ -1,52 +1,105 @@
-// Chose to use a class so that we can instantiate multiple instances of elevators doing different activities
 class Elevator {
-  // Elevator data (e.g., for mulitple elevators)
+  direction = "";
+  destinations = [];
 
   constructor(currentFloor) {
     this.currentFloor = currentFloor;
-    this.destinations = [];
-    this.time = 0;
   }
 
-  // Queues elevator to go to given floor number.
-  goToFloor(floor) {
-    this.destinations.push(floor);
-  }
+  getDestination(floor, button) {
+    this.getDirection(floor);
 
-  printFloorTest(n) {
-    for (let i = 1; i <= n; i++) {
-      setTimeout(() => {
-        console.log(i)
-      }, i * 1000);
+    // HANDLES SCENARIO WITH MULTIPLE FLOORS IN SAME DIRECTION
+    if (this.destinations.length) {
+      if (button === "up" && floor > this.destinations[0]) {
+        this.destinations.push(floor);
+      } else if (button === "down" && floor < this.destinations[0]) {
+        this.destinations.unshift(floor);
+      } else {
+        this.destinations.push(floor)
+      }
+    } else {
+      this.destinations.push(floor);
     }
-  }
+    console.log("Queue: ", this.destinations);
 
-  // Gets the direction the elevator is currently going to move toward (e.g., up or down)
-  getDirection() {
-    if (
-      this.destinations.length === 0 ||
-      this.destinations[0] === this.currentFloor
-    )
-      return "idle";
-    else return this.destinations[0] > this.currentFloor ? "up" : "down";
-  }
-
-  getDirectionToFloor(floor) {
-    if (floor === this.currentFloor) return "idle"
-    else return (floor > this.currentFloor) ? "up" : "down"
-  }
-
-  // Calculates the time required to go to a specific floor
-  getTimeToFloor(floor) {
-    this.time = Math.abs(this.currentFloor - floor);
-    return this.time;
+    // HANDLES GIVEN SCENARIO WITH UPS AND DOWNS
+    // if (this.destinations.length) {
+    //   if (this.direction === "up" && floor > this.destinations[0]) {
+    //     this.destinations.unshift(floor);
+    //   } else if (this.direction === "down" && floor > this.destinations[0]) {
+    //     this.destinations.unshift(floor);
+    //   } else {
+    //     this.destinations.push(floor)
+    //   }
+    // } else {
+    //   this.destinations.push(floor);
+    // }
+    // console.log("Queue: ", this.destinations);
   }
 
   travel() {
-    setInterval()
+    if (this.direction === "up") {
+      for (let i = this.currentFloor; i < this.destinations[0]; i++) {
+        this.currentFloor++;
+      }
+    } else if (this.direction === "down") {
+      for (let i = this.currentFloor; i > this.destinations[0]; i--) {
+        this.currentFloor--
+      }
+    }
   }
 
+  handlePassengers() {
+    this.destinations.shift();
+    console.log("Queue: ", this.destinations)
+  }
 
+  getDirection(floor) {
+    return floor > this.currentFloor ? this.direction = "up" : this.direction = "down";
+  }
 }
 
 module.exports = Elevator;
+
+// getDestination(floor) {
+//   this.destinations.push(floor);
+// }
+
+// goToFloor() {
+//   // TO DO:  FIGURE OUT HOW TO HANDLE THIS PROMISE
+//   let travelTime = new Promise((resolve, reject) => {
+//     resolve(this.travel());
+//   });
+//   travelTime.then(() => console.log(`Arrived at Floor ${this.destinations[0]}`));
+//   this.currentFloor = this.destinations[0];
+//   this.destinations.shift();
+//   // console.log("Time: ", this.time);
+// }
+
+// travel() {
+//   this.getDirection();
+//   this.time++;
+
+//   if (this.direction === "up") {
+//     console.log("Going up!");
+//     for (let i = this.currentFloor; i < this.destinations[0]; i++) {
+
+//     }
+//   } else {
+//     console.log("Going down!");
+//     for (let i = this.currentFloor; i >= this.destinations[0]; i--) {
+
+//     }
+//   }
+// }
+
+// pressUpButton() {
+//   this.upButtonPressed = true;
+//   console.log("Up Button: ", this.upButtonPressed)
+// }
+
+// pressDownButton() {
+//   this.downButtonPressed = true;
+//   console.log("Down Button: ", this.downButtonPressed)
+// }
--- example.js
+++ example.js
@@ -1,17 +1,53 @@
 const Elevator = require('./elevator')
-const Floor = require('./floor')
 
 let elevator = new Elevator;
-let thirdFloor = new Floor(3, false, false);
 
-// Down button pressed on floor three with a passenger wanting to go to floor 2
+// Elevator is at floor 1
+// Passenger on floor 3 makes a request
+// 3 is added to elevator queue
+// Elevator checks direction and assigns an up or down variable. (in this case up)
+// Elevator begins traveling.  Time increments by one per each floor.  Current floor increments by one per each floor.
+// At T1 (first floor), Passenger on floor 10 makes a request
+// Elevator checks to see if:
+    // If elevator direction is equal to down and elevator floor is greater than request floor
+// If queue is empty, reset time to 0;
 
-// Set the current floor where the elevator is located
-elevator.currentFloor = 1;
 
-// Get destination
-elevator.goToFloor(3);
+// CURRENT LOGIC WORKS FOR GIVEN SCENARIO
 
-elevator.printFloorTest(3);
+// NEW SCENARIO TO TEST
+// Elevator at 1
+// Request at 6 to go up to 10
+// Request at 8 to go up to 12
+
+// NEW SCENARIO TO TEST - MULTIPLE PASSENGERS ENTERING MULTIPLE DESTINATIONS
+
+// NEED TO TEST STARTING AT TOP FLOOR
+
+// Elevator is at floor 1
+elevator.currentFloor = 10;
+
+elevator.getDestination(7, "down");
+elevator.getDestination(3, "down");
+elevator.travel();
+elevator.handlePassengers();
+
+
+console.log("Current floor: ", elevator.currentFloor);
+
+elevator.getDestination(1);
+
+elevator.travel();
+elevator.handlePassengers();
+
+console.log("Current floor: ", elevator.currentFloor);
+
+elevator.getDestination(1);
+
+elevator.travel();
+elevator.handlePassengers();
+
+console.log("Current floor: ", elevator.currentFloor);
+
 
 
