550598c4b721f9f63db175709cff2535f7b7bb93
Natalia Malesa <nmalesa@gmail.com>
2022-10-21 08:55:43 -0500

    fix: Test initial floor other than lobby

--- elevator.test.js
+++ elevator.test.js
@@ -8,6 +8,7 @@ beforeEach(() => {
 });
 
 test("handles passengers going down", () => {
+  elevator.currentFloor = 1;
   elevator.getPassengersGoingDown(new Passenger(3, 2, "DOWN", "OUT"));
   elevator.getPassengersGoingDown(new Passenger(10, 1, "DOWN", "OUT"));
   elevator.travel();
@@ -16,6 +17,7 @@ test("handles passengers going down", () => {
 });
 
 test("handles multiple passengers going up", () => {
+  elevator.currentFloor = 1;
   elevator.getPassengersGoingUp(new Passenger(4, 6, "UP", "OUT"));
   elevator.getPassengersGoingUp(new Passenger(8, 10, "UP", "OUT"));
   elevator.travel();
@@ -24,6 +26,7 @@ test("handles multiple passengers going up", () => {
 });
 
 test("handles additional passenger inside elevator going down", () => {
+  elevator.currentFloor = 1;
   elevator.getPassengersGoingDown(new Passenger(3, 2, "DOWN", "OUT"));
   elevator.getPassengersGoingDown(new Passenger(10, 1, "DOWN", "OUT"));
   elevator.getPassengersGoingDown(new Passenger(10, 7, "DOWN", "IN"));
@@ -33,6 +36,7 @@ test("handles additional passenger inside elevator going down", () => {
 });
 
 test("handles additional passenger inside elevator going up", () => {
+  elevator.currentFloor = 1;
   elevator.getPassengersGoingUp(new Passenger(4, 6, "UP", "OUT"));
   elevator.getPassengersGoingUp(new Passenger(8, 10, "UP", "OUT"));
   elevator.getPassengersGoingUp(new Passenger(8, 9, "UP", "IN"));
@@ -42,6 +46,7 @@ test("handles additional passenger inside elevator going up", () => {
 });
 
 test("handles passengers going in multiple directions", () => {
+  elevator.currentFloor = 1;
   elevator.getPassengersGoingUp(new Passenger(3, 7, "UP", "OUT"));
   elevator.getPassengersGoingDown(new Passenger(6, 1, "DOWN", "OUT"));
   elevator.getPassengersGoingDown(new Passenger(6, 3, "DOWN", "IN"));
@@ -49,3 +54,12 @@ test("handles passengers going in multiple directions", () => {
   expect(elevator.testCurrentFloors).toEqual([3, 7, 6, 3, 1]);
   expect(elevator.currentFloor).toBe(1);
 });
+
+test('handles travel when beginning from a floor other than the lobby', () => {
+  elevator.currentFloor = 6;
+  elevator.getPassengersGoingDown(new Passenger(3, 2, "DOWN", "OUT"));
+  elevator.getPassengersGoingDown(new Passenger(10, 1, "DOWN", "OUT"));
+  elevator.travel();
+  expect(elevator.testCurrentFloors).toEqual([10, 3, 2, 1]);
+  expect(elevator.currentFloor).toBe(1);
+})
